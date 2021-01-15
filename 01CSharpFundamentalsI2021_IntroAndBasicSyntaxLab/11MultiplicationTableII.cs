bbf97af5fdf8791cff3e650b7242f25494d136c0
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-15 21:02:48 +0200

    Create 11MultiplicationTableII.cs

---
+++ 01CSharpFundamentalsI2021_IntroAndBasicSyntaxLab/11MultiplicationTableII.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace _11MultiplicationTableII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine($"{integer} X {multiplier} = {integer * multiplier}");
+                multiplier++;
+            } while (multiplier <= 10);            
+        }
+    }
+}
