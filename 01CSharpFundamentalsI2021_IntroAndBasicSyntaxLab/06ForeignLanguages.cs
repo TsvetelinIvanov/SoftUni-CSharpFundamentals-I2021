e01c4a09dfced99bf9ad6326228e45dcecb3846f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-15 19:20:31 +0200

    Create 06ForeignLanguages.cs

---
+++ 01CSharpFundamentalsI2021_IntroAndBasicSyntaxLab/06ForeignLanguages.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace _06ForeignLanguages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            string language = string.Empty;
+            switch (country)
+            {
+                case "England":                    
+                case "USA":
+                    language = "English";
+                    break;
+                case "Spain":                   
+                case "Argentina":                    
+                case "Mexico":
+                    language = "Spanish";
+                    break;                
+                default:
+                    language = "unknown";
+                    break;
+            }
+
+            Console.WriteLine(language);
+        }
+    }
+}
