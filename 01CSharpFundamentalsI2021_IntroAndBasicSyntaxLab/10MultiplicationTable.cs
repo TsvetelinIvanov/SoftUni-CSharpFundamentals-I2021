6088611157e42bd052dd1b8b0d1d64ef299eeca8
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-15 21:01:24 +0200

    Create 10MultiplicationTable.cs

---
+++ 01CSharpFundamentalsI2021_IntroAndBasicSyntaxLab/10MultiplicationTable.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _10MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{integer} X {i} = {integer * i}");
+            }
+        }
+    }
+}
