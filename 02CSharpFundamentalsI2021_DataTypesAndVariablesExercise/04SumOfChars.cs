76b09a3e4eb873d148bf9981299512987e070c6e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-18 21:41:57 +0200

    Create 04SumOfChars.cs

---
+++ 02CSharpFundamentalsI2021_DataTypesAndVariablesExercise/04SumOfChars.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace _04SumOfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charactersCount = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < charactersCount; i++)
+            {
+                char character = char.Parse(Console.ReadLine());
+                sum += character;
+            }
+
+            Console.WriteLine("The sum equals: " + sum);
+        }
+    }
+}
