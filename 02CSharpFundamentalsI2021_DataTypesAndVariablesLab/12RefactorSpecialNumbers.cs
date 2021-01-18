516993b6af834329c801c4a0f6832494c622401b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-18 16:51:27 +0200

    Create 12RefactorSpecialNumbers.cs

---
+++ 02CSharpFundamentalsI2021_DataTypesAndVariablesLab/12RefactorSpecialNumbers.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace _12RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());           
+            for (int i = 1; i <= number; i++)
+            {
+                int currentNumber = i;
+                int digitsSum = 0;
+                while (i > 0)
+                {
+                    digitsSum += i % 10;
+                    i = i / 10;
+                }
+
+                i = currentNumber;
+                bool isSpecialNumber = (digitsSum == 5) || (digitsSum == 7) || (digitsSum == 11);
+                Console.WriteLine("{0} -> {1}", currentNumber, isSpecialNumber);               
+            }
+        }
+    }
+}
