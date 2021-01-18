987465bfc285a0c299f792688eea28859ab316db
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-18 21:32:33 +0200

    Create 03Elevator.cs

---
+++ 02CSharpFundamentalsI2021_DataTypesAndVariablesExercise/03Elevator.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace _03Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            int elevatorCapacity = int.Parse(Console.ReadLine());
+            int coursesCount = (int)Math.Ceiling((double)peopleCount / elevatorCapacity);
+            Console.WriteLine(coursesCount);
+        }
+    }
+}
