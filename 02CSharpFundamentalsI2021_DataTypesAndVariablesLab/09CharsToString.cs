471ded7b1abed327ceba9eedd54626f0eedd2b7e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-18 16:34:06 +0200

    Create 09CharsToString.cs

---
+++ 02CSharpFundamentalsI2021_DataTypesAndVariablesLab/09CharsToString.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _09CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+            char char3 = char.Parse(Console.ReadLine());
+            string str = string.Empty + char1 + char2 + char3;
+            Console.WriteLine(str);
+        }
+    }
+}
