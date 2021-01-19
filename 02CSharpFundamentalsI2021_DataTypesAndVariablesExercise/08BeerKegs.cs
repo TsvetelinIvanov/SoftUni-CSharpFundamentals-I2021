df92adc987ed6c8ad9a34f9ceae3faa7974d89cc
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-19 03:09:15 +0200

    Create 08BeerKegs.cs

---
+++ 02CSharpFundamentalsI2021_DataTypesAndVariablesExercise/08BeerKegs.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace _08BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string biggestKegModel = string.Empty;
+            double biggestKegVolume = 0;
+            int kegsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < kegsCount; i++)
+            {
+                string kegModel = Console.ReadLine();
+                double kegRadius = double.Parse(Console.ReadLine());
+                int kegHeight = int.Parse(Console.ReadLine());
+                double kegVolume = Math.PI * Math.Pow(kegRadius, 2) * kegHeight;
+                if (kegVolume > biggestKegVolume)
+                {
+                    biggestKegVolume = kegVolume;
+                    biggestKegModel = kegModel;
+                }
+            }
+
+            Console.WriteLine(biggestKegModel);
+        }
+    }
+}
