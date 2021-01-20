0b38332c0566b33dac0c14f098890faa33c99f4b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-21 01:33:45 +0200

    Create 10LadyBugs.cs

---
+++ 03CSharpFundamentalsI2021_ArraysExercise/10LadyBugs.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Linq;
+
+namespace _10LadyBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] ladybugIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] field = new int[fieldSize];
+            foreach (int ladybugIndex in ladybugIndexes)
+            {
+                if (ladybugIndex >= 0 && ladybugIndex < fieldSize)
+                {
+                    field[ladybugIndex] = 1;
+                }
+            }
+
+            string commandString;
+            while ((commandString = Console.ReadLine()) != "end")
+            {
+                string[] commandArray = commandString.Split();
+                int index = int.Parse(commandArray[0]);
+                string direction = commandArray[1];
+                int distance = int.Parse(commandArray[2]);
+
+                if (index >= 0 && index < fieldSize && field[index] == 1)
+                {
+                    field[index] = 0;
+                    switch (direction)
+                    {
+                        case "right":
+                            index += distance;
+                            break;
+                        case "left":
+                            index -= distance;
+                            break;
+                    }
+
+                    while (index >= 0 && index < fieldSize && field[index] == 1)
+                    {
+                        switch (direction)
+                        {
+                            case "right":
+                                index += distance;
+                                break;
+                            case "left":
+                                index -= distance;
+                                break;
+                        }
+                    }
+
+                    if (index >= 0 && index < fieldSize)
+                    {
+                        field[index] = 1;
+                    }
+                }                
+            }
+
+            Console.WriteLine(string.Join(" ", field));
+        }
+    }
+}
