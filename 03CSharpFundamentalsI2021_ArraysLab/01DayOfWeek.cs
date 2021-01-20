f3945b24ebedfee70bc3202d1b75423487e80856
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-20 10:48:19 +0200

    Create 01DayOfWeek.cs

---
+++ 03CSharpFundamentalsI2021_ArraysLab/01DayOfWeek.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace _01DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            int dayNumber = int.Parse(Console.ReadLine());
+            if (dayNumber >= 1 && dayNumber <= 7)
+            {
+                Console.WriteLine(days[dayNumber - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
