c3b8d0f3fa2f0b01a0625dbba8bb616fc17320f4
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-23 22:54:24 +0200

    Create 10TopNumber.cs

---
+++ 04CSharpFundamentalsI2021_MethodsExercise/10TopNumber.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace _10TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int endNumber = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= endNumber; i++)
+            {
+                bool isTopNumber = CheckTopNumber(i);
+                if (isTopNumber)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static bool CheckTopNumber(int number)
+        {
+            int digitsSum = 0;
+            bool haveOddDigit = false;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                number /= 10;
+                digitsSum += digit;
+                if (digit % 2 != 0)
+                {
+                    haveOddDigit = true;
+                }
+            }
+
+            return digitsSum % 8 == 0 && haveOddDigit;
+        }
+    }
+}
