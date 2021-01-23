a70c36e63308e763965464202017b44fd77b2214
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-23 20:52:20 +0200

    Create 06MiddleCharacters.cs

---
+++ 04CSharpFundamentalsI2021_MethodsExercise/06MiddleCharacters.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace _06MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintMiddleCharacters(input);
+        }
+
+        private static void PrintMiddleCharacters(string input)
+        {
+            if (input.Length % 2 == 0)
+            {
+                Console.WriteLine("" + input[input.Length / 2 - 1] + input[input.Length / 2]);
+            }
+            else
+            {
+                Console.WriteLine(input[input.Length / 2]);
+            }
+        }
+    }
+}
