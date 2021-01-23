02387955463aeba278b3e5d5e8c78bf0f3c68091
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-23 21:21:36 +0200

    Create 08FactorialDivision.cs

---
+++ 04CSharpFundamentalsI2021_MethodsExercise/08FactorialDivision.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace _08FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            long firstFactorial = GetFactorial(firstNumber);
+            long secondFactorial = GetFactorial(secondNumber);
+            decimal result = (decimal)firstFactorial / secondFactorial;
+            Console.WriteLine($"{result:f2}");
+        }
+
+        private static long GetFactorial(int number)
+        {
+            long factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
