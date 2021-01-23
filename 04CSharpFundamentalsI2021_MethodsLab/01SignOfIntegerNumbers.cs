07f34e3ffeeb41bfcb101b0fad1467dbc078615a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-23 11:52:10 +0200

    Create 01SignOfIntegerNumbers.cs

---
+++ 04CSharpFundamentalsI2021_MethodsLab/01SignOfIntegerNumbers.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace _01SignOfIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintSign(number);
+        }
+
+        private static void PrintSign(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
