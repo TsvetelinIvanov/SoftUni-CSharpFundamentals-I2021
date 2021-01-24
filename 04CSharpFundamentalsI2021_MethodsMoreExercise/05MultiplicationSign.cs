62a8d0a498b91d30760e824082893291f6d400b9
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-24 21:51:03 +0200

    Create 05MultiplicationSign.cs

---
+++ 04CSharpFundamentalsI2021_MethodsMoreExercise/05MultiplicationSign.cs
@@ -0,0 +1,77 @@
+using System;
+
+namespace _05MultiplicationSign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            string productSign = CheckProductSign(firstNumber, secondNumber, thirdNumber);
+            Console.WriteLine(productSign);
+        }
+
+        private static string CheckProductSign(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            string productSign = string.Empty;
+            if (firstNumber == 0 || secondNumber == 0 || thirdNumber == 0)
+            {
+                productSign = "zero";
+            }
+            else if (firstNumber < 0)
+            {
+                if (secondNumber < 0)
+                {
+                    if (thirdNumber < 0)
+                    {
+                        productSign = "negative";
+                    }
+                    else if (thirdNumber > 0)
+                    {
+                        productSign = "positive";
+                    }
+                }
+                else if (secondNumber > 0)
+                {
+                    if (thirdNumber > 0)
+                    {
+                        productSign = "negative";
+                    }
+                    else if (thirdNumber < 0)
+                    {
+                        productSign = "positive";
+                    }
+                }
+            }
+            else if (firstNumber > 0)
+            {
+                if (secondNumber < 0)
+                {
+                    if (thirdNumber > 0)
+                    {
+                        productSign = "negative";
+                    }
+                    else if (thirdNumber < 0)
+                    {
+                        productSign = "positive";
+                    }
+                }
+                else if (secondNumber > 0)
+                {
+                    if (thirdNumber < 0)
+                    {
+                        productSign = "negative";
+                    }
+                    else if (thirdNumber > 0)
+                    {
+                        productSign = "positive";
+                    }
+                }
+            }
+
+            return productSign;
+        }
+    }
+}
