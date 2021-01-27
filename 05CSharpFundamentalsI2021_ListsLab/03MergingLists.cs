4fec10a9a7a5ff48b2752e5be6d823b1616fadd3
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-01-27 10:52:54 +0200

    Create 03MergingLists.cs

---
+++ 05CSharpFundamentalsI2021_ListsLab/03MergingLists.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03MergingLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> firstList = Console.ReadLine().Split().ToList();
+            List<string> secondList = Console.ReadLine().Split().ToList();
+            List<string> mergedList = new List<string>();
+            for (int i = 0; i < Math.Min(firstList.Count, secondList.Count); i++)
+            {
+                mergedList.Add(firstList[i]);
+                mergedList.Add(secondList[i]);
+            }
+
+            if (firstList.Count > secondList.Count)
+            {
+                string[] remainigElements = GetRemiainingElements(firstList, secondList);
+                mergedList.AddRange(remainigElements);
+            }
+            else if (secondList.Count > firstList.Count)
+            {
+                string[] remainigElements = GetRemiainingElements(secondList, firstList);
+                mergedList.AddRange(remainigElements);
+            }
+
+            Console.WriteLine(string.Join(" ", mergedList));
+        }
+
+        private static string[] GetRemiainingElements(List<string> longerList, List<string> shorterList)
+        {
+            List<string> remainingElements = new List<string>();
+            for (int i = shorterList.Count; i < longerList.Count; i++)
+            {
+                remainingElements.Add(longerList[i]);
+            }
+
+            return remainingElements.ToArray();
+        }
+    }
+}
