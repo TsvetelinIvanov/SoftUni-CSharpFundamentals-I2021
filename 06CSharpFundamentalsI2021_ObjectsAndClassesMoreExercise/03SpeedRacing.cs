9c4eb46346a4bc26f8143c3030c3788892e406a1
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-01 16:11:30 +0200

    Create 03SpeedRacing.cs

---
+++ 06CSharpFundamentalsI2021_ObjectsAndClassesMoreExercise/03SpeedRacing.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+
+namespace _03SpeedRacing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            int carsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < carsCount; i++)
+            {
+                string[] carData = Console.ReadLine().Split();
+                string model = carData[0];
+                double fuel = double.Parse(carData[1]);
+                double consumptionPerKilometer = double.Parse(carData[2]);
+                Car car = new Car(model, fuel, consumptionPerKilometer);
+                cars.Add(car);
+            }
+
+            string driveData;
+            while ((driveData = Console.ReadLine()) != "End")
+            {
+                string model = driveData.Split()[1];
+                int distance = int.Parse(driveData.Split()[2]);
+                Car car = cars.Find(c => c.Model == model);
+                car.Drive(distance);
+            }
+
+            cars.ForEach(c => Console.WriteLine(c));
+        }
+    }
+
+    class Car
+    {
+        public Car(string model, double fuelAmount, double consumptionPerKilometer)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.ConsumtionPerKilometer = consumptionPerKilometer;
+            this.TraveledDistance = 0;
+        }
+
+        public string Model { get; }
+
+        public double FuelAmount { get; private set; }
+
+        public double ConsumtionPerKilometer { get; }
+
+        public int TraveledDistance { get; private set; }
+
+        public void Drive(int distance)
+        {
+            if (this.ConsumtionPerKilometer * distance <= this.FuelAmount)
+            {
+                this.FuelAmount -= this.ConsumtionPerKilometer * distance;
+                this.TraveledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Model} {this.FuelAmount:f2} {this.TraveledDistance}";
+        }
+    }
+}
