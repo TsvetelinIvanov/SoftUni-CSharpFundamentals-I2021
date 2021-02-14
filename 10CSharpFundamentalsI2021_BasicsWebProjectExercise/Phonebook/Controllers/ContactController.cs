17562de10771f7c0163fce072e03d279228db2f7
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-14 21:02:15 +0200

    Add 10CSharpFundamentalsI2021_BasicsWebProjectExercise

---
+++ 10CSharpFundamentalsI2021_BasicsWebProjectExercise/Phonebook/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text.RegularExpressions;
+using Phonebook.Data;
+using Phonebook.Data.Models;
+
+namespace Phonebook.Controllers
+{
+    public class ContactController : Controller
+    {
+        private const string InvalidNameExceptionmessage = "The name must start with capital letter and have only letters, digits, white spaces and _!";
+        private const string InvalidNumberExceptionmessage = "The number must have only digits and may start with +!";
+
+        [HttpPost]
+        public IActionResult Create(Contact contact)
+        {
+            Regex nameRegex = new Regex(@"^[A-Z][\w ]+$");
+            Regex numberRegex = new Regex(@"^\+?\d+$");
+
+            if (!nameRegex.IsMatch(contact.Name))
+            {
+                throw new ArgumentException(InvalidNameExceptionmessage);
+            }
+
+            if (!numberRegex.IsMatch(contact.Number))
+            {
+                throw new ArgumentException(InvalidNumberExceptionmessage);
+            }
+
+            DataAccess.Contacts.Add(contact);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ 10CSharpFundamentalsI2021_BasicsWebProjectExercise/Phonebook/Data/DataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Phonebook.Data.Models;
+
+namespace Phonebook.Data
+{
+    public class DataAccess
+    {
+        static DataAccess()
+        {
+            Contacts = new List<Contact>();
+        }
+
+        public static List<Contact> Contacts { get; set; }
+    }
+}
