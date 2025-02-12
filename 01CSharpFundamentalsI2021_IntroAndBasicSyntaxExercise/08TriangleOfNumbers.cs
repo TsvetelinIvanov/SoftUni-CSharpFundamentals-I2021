30782dac64b2cec3a867bbdaa9da48e9fa891bbc
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-02-12 18:24:23 +0200

    Update 08TriangleOfNumbers.cs

--- 01CSharpFundamentalsI2021_IntroAndBasicSyntaxExercise/08TriangleOfNumbers.cs
+++ 01CSharpFundamentalsI2021_IntroAndBasicSyntaxExercise/08TriangleOfNumbers.cs
@@ -6,8 +6,8 @@ namespace _08TriangleOfNumbers
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= n; i++)
+            int numbersCount = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= numbersCount; i++)
             {
                 for (int j = 1; j <= i; j++)
                 {
