0d2d2633ce789e2511690178f8b2e95b26c981d3
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-02-12 18:30:55 +0200

    Update 09PadawanEquipment.cs

--- 01CSharpFundamentalsI2021_IntroAndBasicSyntaxExercise/09PadawanEquipment.cs
+++ 01CSharpFundamentalsI2021_IntroAndBasicSyntaxExercise/09PadawanEquipment.cs
@@ -8,16 +8,15 @@ namespace _09PadawanEquipment
         {
             decimal disposableMoney = decimal.Parse(Console.ReadLine());
             int studentsCount = int.Parse(Console.ReadLine());
-            decimal lightsaberPrice = decimal.Parse(Console.ReadLine());
+            decimal lightSaberPrice = decimal.Parse(Console.ReadLine());
             decimal robePrice = decimal.Parse(Console.ReadLine());
             decimal beltPrice = decimal.Parse(Console.ReadLine());
 
-            decimal allLightsabersPrice = lightsaberPrice * Math.Ceiling(studentsCount * 1.1m);            
+            decimal allLightSabersPrice = lightSaberPrice * Math.Ceiling(studentsCount * 1.1m);            
             decimal allRobesPrice = robePrice * studentsCount;
             decimal allBeltsPrice = beltPrice * (studentsCount - studentsCount / 6);            
 
-            decimal allItemsPrice = allLightsabersPrice + allRobesPrice + allBeltsPrice;                        
-
+            decimal allItemsPrice = allLightSabersPrice + allRobesPrice + allBeltsPrice;
             if (allItemsPrice <= disposableMoney)
             {
                 Console.WriteLine($"The money is enough - it would cost {allItemsPrice:f2}lv.");
