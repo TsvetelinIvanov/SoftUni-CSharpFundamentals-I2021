6ceb0b7f3adee45f155e035d87f8ea41f242a814
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-02-23 14:36:34 +0200

    Update Program.cs

--- 05CSharpFundamentalsI2021_BitwiseOperationsLab/03PthBit/Program.cs
+++ 05CSharpFundamentalsI2021_BitwiseOperationsLab/03PthBit/Program.cs
@@ -9,6 +9,7 @@ namespace _03PthBit
             int number = int.Parse(Console.ReadLine());
             int position = int.Parse(Console.ReadLine());
             int mask = 1;
+            
             int currentNumber = number >> position;
             int bitAtPositionP = currentNumber & mask;
             Console.WriteLine(bitAtPositionP);
