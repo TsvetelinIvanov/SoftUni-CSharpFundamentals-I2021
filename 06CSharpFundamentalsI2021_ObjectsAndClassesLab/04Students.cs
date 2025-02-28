d1f1bedd9ba3adadffd4821f34e0a6791cb48caa
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-02-28 10:15:22 +0200

    Update 04Students.cs

--- 06CSharpFundamentalsI2021_ObjectsAndClassesLab/04Students.cs
+++ 06CSharpFundamentalsI2021_ObjectsAndClassesLab/04Students.cs
@@ -4,6 +4,25 @@ using System.Collections.Generic;
 
 namespace _04Students
 {
+    class Student
+    {
+        public Student(string firstName, string lastName, int age, string homeTown)
+        {
+            this.FirtName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.HomeTown = homeTown;
+        }
+
+        public string FirtName { get; }
+        
+        public string LastName { get; }
+        
+        public int Age { get; }
+        
+        public string HomeTown { get; }
+    }
+    
     class Program
     {
         static void Main(string[] args)
@@ -17,6 +36,7 @@ namespace _04Students
                 string lastName = studentData[1];
                 int age = int.Parse(studentData[2]);
                 string homeTown = studentData[3];
+                
                 Student student = new Student(firstName, lastName, age, homeTown);
                 students.Add(student);
             }
@@ -24,21 +44,5 @@ namespace _04Students
             string town = Console.ReadLine();
             students.Where(s => s.HomeTown == town).ToList().ForEach(s => Console.WriteLine($"{s.FirtName} {s.LastName} is {s.Age} years old."));
         }
-    }
-
-    class Student
-    {
-        public Student(string firstName, string lastName, int age, string homeTown)
-        {
-            this.FirtName = firstName;
-            this.LastName = lastName;
-            this.Age = age;
-            this.HomeTown = homeTown;
-        }
-
-        public string FirtName { get; }
-        public string LastName { get; }
-        public int Age { get; }
-        public string HomeTown { get; }
     }
 }
