423d72b31267dc80c6959e8fb1968274c8c870da
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-03-03 12:16:01 +0200

    Update Program.cs

--- 07CSharpFundamentalsI2021_AssociativeArraysExercise/07StudentAcademy/Program.cs
+++ 07CSharpFundamentalsI2021_AssociativeArraysExercise/07StudentAcademy/Program.cs
@@ -8,23 +8,24 @@ namespace _07StudentAcademy
     {
         static void Main(string[] args)
         {
-            Dictionary<string, List<double>> studentsGrades = new Dictionary<string, List<double>>();
+            Dictionary<string, List<double>> studentsWithGrades = new Dictionary<string, List<double>>();
+            
             int gradesCount = int.Parse(Console.ReadLine());
             for (int i = 0; i < gradesCount; i++)
             {
                 string name = Console.ReadLine();
                 double grade = double.Parse(Console.ReadLine());
-                if (!studentsGrades.ContainsKey(name))
+                if (!studentsWithGrades.ContainsKey(name))
                 {
-                    studentsGrades.Add(name, new List<double>());
+                    studentsWithGrades.Add(name, new List<double>());
                 }
 
-                studentsGrades[name].Add(grade);
+                studentsWithGrades[name].Add(grade);
             }
 
-            foreach (KeyValuePair<string, List<double>> student in studentsGrades.Where(s => s.Value.Average() >= 4.5).OrderByDescending(s => s.Value.Average()))
+            foreach (KeyValuePair<string, List<double>> studentWithGrades in studentsWithGrades.Where(s => s.Value.Average() >= 4.5).OrderByDescending(s => s.Value.Average()))
             {
-                Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+                Console.WriteLine($"{studentWithGrades.Key} -> {studentWithGrades.Value.Average():f2}");
             }
         }
     }
