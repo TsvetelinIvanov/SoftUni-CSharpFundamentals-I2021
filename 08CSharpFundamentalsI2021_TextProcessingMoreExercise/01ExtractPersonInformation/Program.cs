d8ee597b5434c0bebbe93db2f0e329bd4a350190
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-03-07 00:20:13 +0200

    Update Program.cs

--- 08CSharpFundamentalsI2021_TextProcessingMoreExercise/01ExtractPersonInformation/Program.cs
+++ 08CSharpFundamentalsI2021_TextProcessingMoreExercise/01ExtractPersonInformation/Program.cs
@@ -7,19 +7,19 @@ namespace _01ExtractPersonInformation
         static void Main(string[] args)
         {
             int linesCount = int.Parse(Console.ReadLine());
-            char nameStartChar = '@';
-            char nameEndChar = '|';
-            char ageStartChar = '#';
-            char ageEndChar = '*';
+            char nameStartCharacter = '@';
+            char nameEndCharacter = '|';
+            char ageStartCharacter = '#';
+            char ageEndCharacter = '*';
             for (int i = 0; i < linesCount; i++)
             {
                 string line = Console.ReadLine();
-                int nameStartIndex = line.IndexOf(nameStartChar) + 1;
-                int nameEndIndex = line.IndexOf(nameEndChar);
+                int nameStartIndex = line.IndexOf(nameStartCharacter) + 1;
+                int nameEndIndex = line.IndexOf(nameEndCharacter);
                 string name = line.Substring(nameStartIndex, nameEndIndex - nameStartIndex);
 
-                int ageStartIndex = line.IndexOf(ageStartChar) + 1;
-                int ageEndIndex = line.IndexOf(ageEndChar);
+                int ageStartIndex = line.IndexOf(ageStartCharacter) + 1;
+                int ageEndIndex = line.IndexOf(ageEndCharacter);
                 string age = line.Substring(ageStartIndex, ageEndIndex - ageStartIndex);
 
                 Console.WriteLine($"{name} is {age} years old.");
