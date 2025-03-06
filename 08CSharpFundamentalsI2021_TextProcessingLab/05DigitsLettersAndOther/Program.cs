cddb1e5867ce0e21d21ac7c0c752008dffe4cf38
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-03-06 15:28:33 +0200

    Update Program.cs

--- 08CSharpFundamentalsI2021_TextProcessingLab/05DigitsLettersAndOther/Program.cs
+++ 08CSharpFundamentalsI2021_TextProcessingLab/05DigitsLettersAndOther/Program.cs
@@ -9,6 +9,7 @@ namespace _05DigitsLettersAndOther
             string digitsString = string.Empty;
             string lettersString = string.Empty;
             string otherCharactersString = string.Empty;
+            
             string input = Console.ReadLine();
             for (int i = 0; i < input.Length; i++)
             {
