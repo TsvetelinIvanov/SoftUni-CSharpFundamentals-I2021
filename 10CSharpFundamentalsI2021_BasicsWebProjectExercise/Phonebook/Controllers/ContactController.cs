2cd81fe22d6b1090439b337e3a6992b9b8edd400
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-03-10 14:54:50 +0200

    Update ContactController.cs

--- 10CSharpFundamentalsI2021_BasicsWebProjectExercise/Phonebook/Controllers/ContactController.cs
+++ 10CSharpFundamentalsI2021_BasicsWebProjectExercise/Phonebook/Controllers/ContactController.cs
@@ -8,7 +8,7 @@ namespace Phonebook.Controllers
 {
     public class ContactController : Controller
     {
-        private const string InvalidNameExceptionmessage = "The name must start with capital letter and have only letters, digits, white spaces and _!";
+        private const string InvalidNameExceptionmessage = "The name must start with a capital letter and contains only letters, digits, white spaces and _!";
         private const string InvalidNumberExceptionmessage = "The number must have only digits and may start with +!";
 
         [HttpPost]
